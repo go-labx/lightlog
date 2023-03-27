20dbbc104be5292905f5f91a39a795a126ae5516
wanggan.kk <wanggan.kk@bytedance.com>
2023-03-27 19:00:14 +0800

    feat: Console transport supports outputting colored text.

--- transport_console.go
+++ transport_console.go
@@ -1,6 +1,9 @@
 package lightlog
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-labx/color"
+)
 
 type ConsoleTransport struct {
 	*Transport
@@ -12,6 +15,19 @@ func NewConsoleTransport(name string, level Level) *ConsoleTransport {
 	}
 }
 
-func (c *ConsoleTransport) Log(formattedData string, _ *LogData) {
-	fmt.Println(formattedData)
+func (c *ConsoleTransport) Log(formattedData string, data *LogData) {
+	switch data.level {
+	case TRACE:
+		fmt.Println(color.CyanString(formattedData))
+	case DEBUG:
+		fmt.Println(color.BlueString(formattedData))
+	case INFO:
+		fmt.Println(color.GreenString(formattedData))
+	case WARN:
+		fmt.Println(color.YellowString(formattedData))
+	case ERROR, FATAL:
+		fmt.Println(color.RedString(formattedData))
+	default:
+		fmt.Println(formattedData)
+	}
 }
